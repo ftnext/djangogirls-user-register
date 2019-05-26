66d687aac0a2395dd28569cba95912126241a639
ftnext <takuyafjp+develop@gmail.com>
2019-05-26 15:01:31 +0900

    [chore] blogアプリを作成、インストール設定
    * `python manage.py startapp blog`

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
